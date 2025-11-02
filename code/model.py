f13bb9113420244039dc1bd3a3eab7a34b3461c8
PstormQT <131224632+PstormQT@users.noreply.github.com>
2025-11-02 16:04:40 -0500

    Update new model

---
+++ code/model.py
@@ -0,0 +1,29 @@
+
+
+
+
+class model2048:
+    board = None
+    score = 0
+    state = True
+
+    # Board 4x4 board itself
+    # score = init to 0
+    # state (still playing or dead) = True if still playing, False if it is not
+    def __init__():
+        board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        score = 0
+
+    def getScore() -> int:
+        pass
+
+    def getBoard() -> list:
+        pass
+    
+    # Add a movement to the board. 
+    # 1 for up
+    # 2 for down
+    # 3 for left
+    # 4 for right
+    def moveBoard(move: int):
+        pass
