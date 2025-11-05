0e2b2fc1fec57d570e1563aee6d039410ae2c134
NathanRusso <nathandanielrusso@gmail.com>
2025-11-05 11:13:45 -0500

    I added lots of comments.

--- code/model.py
+++ code/model.py
@@ -2,16 +2,20 @@ from enum import Enum
 import copy, random
 
 class Direction(Enum):
+    """
+    Class to represent the 4 directions of movement.\n
+    UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4.
+    """
     UP = 1
     DOWN = 2
     LEFT = 3
     RIGHT = 4
 
-"""
-Class to represent the board of the 2048 game.
-Board coordinates are represented as (y, x)
-"""
 class Board2048:
+    """
+    Class to represent the board of the 2048 game.\n
+    Board coordinates are represented as (y, x)
+    """
     MAX_BOARD_DIMENSION = 4
     TILE_2_CHANCE = 0.9
     TILE_4_CHANCE = 0.1
@@ -23,10 +27,10 @@ class Board2048:
         self.addTile()
         self.addTile()
 
-    def setBoard(self, board: list):
-        self.board = board
-
     def getAllOpenCells(self):
+        """
+        This returns a list of all open cells in the game board.
+        """
         open_cells = []
         for y in range(self.MAX_BOARD_DIMENSION):
             for x in range(self.MAX_BOARD_DIMENSION):
@@ -35,6 +39,9 @@ class Board2048:
         return open_cells
 
     def addTile(self):
+        """
+        This adds a random tile to the board.
+        """
         open_cells = self.getAllOpenCells()
         if open_cells != []:
             y, x = random.choice(open_cells)
@@ -44,12 +51,18 @@ class Board2048:
             else:
                 self.board[y][x] = 4
 
-    """
-    Row - [Left, Right], Col - [Bottom, Top]
-    This merges and combines numbers from right to right.
-    It start are the right and merges values in the right direction.
-    """
-    def merge(self, list_values):
+    def merge(self, list_values: list):
+        """
+        This merges and combines numbers from right to right for a single row/column.\n
+        It start are the right and merges values in the right direction.\n
+        Row - [Left, Right], Col - [Bottom, Top]
+
+        Args:
+            list_values (list): The row/column list of values to combine
+
+        Returns:
+            list: The updated row/column list of values after the shift/merge.
+        """
         list_values = [v for v in list_values if v != 0]
         final_values = []
         i = len(list_values) - 1
@@ -67,14 +80,18 @@ class Board2048:
         final_values.reverse()
         return final_values
 
-    """
-    This moves the pieces on the board in the desired direction.
-    Returns if the board changed
-    """
     def move(self, direction) -> bool:
+        """
+        This moves the tiles on the board in the desired direction.
+
+        Args:
+            direction (int): The direction to move all of the tiles.
+
+        Returns:
+            bool: If the board changed after trying to move the pieces.
+        """
         if self.game_over:
             print(f"The game is over. Your prior score was {self.score}. You cannot move anymore.")
-            print("Call reset() to start again.")
             return False
 
         original_board = copy.deepcopy(self.board)
@@ -106,7 +123,12 @@ class Board2048:
 
         return original_board != self.board
 
-    def updateGameOver(self):
+    def updateGameOver(self) -> bool:
+        """
+        This checks if there are no more moves to be made on the game board.
+        Returns:
+            bool: True if game is over, False is there are still moves to be made.
+        """
         for row in range(self.MAX_BOARD_DIMENSION):
             for col in range(self.MAX_BOARD_DIMENSION):
                 if (self.board[row][col] == 0):
@@ -120,13 +142,9 @@ class Board2048:
                     return
 
         self.game_over = True
-    
-    def reset(self):
-        self.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-        self.score = 0
-        self.game_over = False
-        self.addTile()
-        self.addTile()
+
+    def setBoard(self, board: list):
+        self.board = board
 
     def getBoard(self) -> list:
         return self.board
@@ -138,6 +156,9 @@ class Board2048:
         return self.game_over
 
     def displayCLI(self):
+        """
+        This displays the current CLI board and score.
+        """
         print("----------------------------------------")
         print(f"Score: {self.score}")
         for i in range(self.MAX_BOARD_DIMENSION):
@@ -146,12 +167,23 @@ class Board2048:
             print(" ")
 
     def playAction(self, direction):
+        """
+        This moves the tiles in a given direction, adds a new tile, and checks if the game is over.
+        Args:
+            direction (int): The direction to move all of the tiles.
+        """
         changed = self.move(direction)
         if (changed):
             self.addTile()
             self.updateGameOver()
     
     def playActionCLI(self, direction):
+        """
+        This moves the tiles in a given direction, adds a new tile, and checks if the game is over.\n
+        This also displays the current CLI board and score. 
+        Args:
+            direction (int): The direction to move all of the tiles.
+        """
         changed = self.move(direction)
         if (changed):
             self.addTile()
